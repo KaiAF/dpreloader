36fea100cd5d555f35f446a71ffca7a8557e285d
iris <60985521+KaiAF@users.noreply.github.com>
2024-02-11 16:55:04 -0600

    add resource pack

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { execSync } = require('child_process');
 const express = require('express');
 const { rateLimit } = require('express-rate-limit');
+const os = require('os');
 const fs = require('fs');
 const app = express();
 
@@ -25,6 +26,25 @@ const MaxRequests = rateLimit({
 });
 
 app.use(MaxRequests);
+
+app.get('/pack.zip', async function (req, res) {
+  try {
+    if (!fs.existsSync('./resourcePack')) return res.sendStatus(404);
+    if (!fs.existsSync('./resourcePack/pack.zip')) {
+      // create new .zip pack
+      if (os.platform() == 'win32') {
+        execSync('cd ./resourcePack && tar.exe -a -c -f pack.zip .');
+      } else {
+        execSync('cd ./resourcePack && zip pack.zip *');
+      }
+    }
+
+    res.sendFile('./resourcePack/pack.zip');
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+});
 
 app.post('/upload', async function (req, res) {
   try {
@@ -51,6 +71,18 @@ app.post('/upload', async function (req, res) {
       console.warn(e);
       execSync(`reload${config.server.isBukkit ? ' confirm' : ''}`);
     }
+    res.json({ OK: true });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ OK: false, e });
+  }
+});
+
+app.post('/upload-resource-pack', async function (req, res) {
+  try {
+    if (fs.existsSync('./resourcePack')) fs.rmSync('./resourcePack', { recursive: true });
+    execSync(`git clone ${config.resourceRepo} ./resourcePack`);
+
     res.json({ OK: true });
   } catch (e) {
     console.error(e);
